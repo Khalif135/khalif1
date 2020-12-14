e1586c043131a8eb18acf797be435222dbe9f42f
Khalif135 <76011176+Khalif135@users.noreply.github.com>
2020-12-15 05:02:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: '👾AR15BOT👾',
+    A187: 'Khalif Fitra',
     instagram: 'https://instagram.com/_sadboy.ig',
-    nomer: 'wa.me/6285722553839',
-    aktif: '24 JAM',
+    nomer: 'wa.me/6281244355770',
+    aktif: 'Jika Di Akhtifkan',
     youtube: 'https://youtube.com/channel/UCGYLWtyT9IADYNUiK0uZiGg',
     whatsapp: 'https://chat.whatsapp.com/DSSHmG2KjKJLoFp9B9mkVs',
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
